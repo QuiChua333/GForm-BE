85dee02ec1900a4ea78bd6145beb295a609dfcac
Milo <milo.huynh.gos@gmail.com>
2024-05-20 07:35:43 +0700

    feat: add api register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { RegisterUserDTO } from './DTO';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('register')
-  async register(@Body() body: RegisterUserDTO) {
+  async register(@Body() body: RegisterUserDTO, @Res() res: Response) {
     try {
       const user = await this.authService.register(body);
-      return user;
+      res.status(HttpStatus.CREATED).json({
+        message: 'Sign in successfully',
+        data: user,
+      });
     } catch (error) {
-      return error;
+      res.status(HttpStatus.BAD_REQUEST).json({
+        error,
+      });
     }
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { RegisterUserDTO } from './DTO';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/Entity/user.entity';
 import { Repository } from 'typeorm';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -10,10 +11,16 @@ export class AuthService {
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  async register(registerDTO: RegisterUserDTO): Promise<User> {
-    return await this.userRepository.save({
+  async register(registerDTO: RegisterUserDTO) {
+    const hashedPassword = await argon.hash(registerDTO.password);
+    const user = await this.userRepository.save({
       ...registerDTO,
+      password: hashedPassword,
       isAdmin: false,
+      isVerifiedEmail: false,
     });
+    delete user.password;
+    delete user.refreshToken;
+    return user;
   }
 }
